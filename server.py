e6b230ee6ee862293c08db267b6aa3f158e23b3d
Evan Klitzke <evan@eklitzke.org>
2011-04-06 17:03:30 -0700

    allow running various bottle servers

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import optparse
 import simplejson
 
+import bottle
 import tornado.web
 import tornado.ioloop
 
@@ -20,19 +21,40 @@ class ResponseGenerator(object):
             self.position = 0
         return obj
 
+response_generator = None
+
 class JSONHandler(tornado.web.RequestHandler):
 
     def get(self):
         self.set_header('Content-Type', 'application/json')
-        response = self.application.response_generator.get_object()
-        self.write(simplejson.dumps(response))
+        self.write(simplejson.dumps(response_generator.get_object()))
+
+@bottle.route('/')
+def bottle_handler():
+    return simplejson.dumps(response_generator.get_object())
 
 if __name__ == '__main__':
+    global response_generator
     parser = optparse.OptionParser()
     parser.add_option('-i', '--input', help='file of lines to input')
     opts, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error('usage: server.py [bottle|tornado]')
 
-    application = tornado.web.Application([('/', JSONHandler)], debug=False)
-    application.listen(8888)
-    application.response_generator = ResponseGenerator(opts.input)
-    tornado.ioloop.IOLoop.instance().start()
+    mode = args[0]
+    if mode not in ('tornado', 'bottle', 'bottle-paste', 'bottle-cherry'):
+        parser.error('usage: server.py [bottle|bottle-cherry|bottle-paste|tornado]')
+
+    response_generator = ResponseGenerator(opts.input)
+    if mode == 'tornado':
+        application = tornado.web.Application([('/', JSONHandler)], debug=False)
+        application.listen(8888)
+        tornado.ioloop.IOLoop.instance().start()
+    elif mode == 'bottle':
+        bottle.run(host='localhost', port=8888)
+    elif mode == 'bottle-paste':
+        bottle.run(server=bottle.PasteServer, host='localhost', port=8888)
+    elif mode == 'bottle-cherry':
+        bottle.run(server=bottle.CherryPyServer, host='localhost', port=8888)
+    else:
+        assert False
